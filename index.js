52b00145a51634f5d5dcd011323be59ca74378ec
adityabanotra <aditya.banotra@gmail.com>
2021-11-30 18:36:50 +0530

    Second Commit

--- index.js
+++ index.js
@@ -5,9 +5,39 @@ const port = 8000;
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));  
+app.use(express.urlencoded()); // this is parser(midleware)used to get data from req(req.body)which is being sent by form in views
+app.use(express.static('assets')); // middleware to give asset path to static files
+var work = [
+    {
+        work : "gym",
+        time : "6AM" 
+    },
+    {
+        work : "shower",
+        time : "8AM" 
+    },
+    {
+        work : "breakfast",
+        time : "8:45AM"
+    }
+];
 
 app.get('/', function(req,res){
-    return res.render('home');
+    return res.render('home',{
+        title:"easyNotes",
+        work : work
+    });
+})
+
+
+
+app.get('/profile', function(req,res){
+    return res.render('profile');
+})
+
+app.post('/add-work', function(req,res){
+    work.push(req.body);
+    return res.redirect('/');
 })
 
 app.listen(port,function(err){
