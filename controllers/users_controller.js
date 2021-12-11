244c38f40ea8c4a467b68627ddf5c6a1aaf9fac3
adityabanotra <aditya.banotra@gmail.com>
2021-12-12 02:24:16 +0530

    create-session done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,8 +5,32 @@ module.exports.login=function(req,res){
     });
 };
 
+module.exports.profile = function(req,res)
+{
+    if(req.cookies.user_id)
+    {
+        User.findById(req.cookies.user_id, function(err,user){
+            
+            if(err)
+            {
+                console.log('Error'.err);
+                return;
+            }
+            
+            return res.render('profile',{
+                title: user.name,
+                user:user
+            });
+        })
+        
+    }
+    else
+    {
+        return res.redirect('user/login');
+    }
+};
 
-module.exports.create=function(req,res){
+module.exports.create = function(req,res){
     if(req.body.password!=req.body.conpass)
     {
         return res.redirect('back');
@@ -26,12 +50,14 @@ module.exports.create=function(req,res){
         }
         else
         {
+            req.body.score=0;
             User.create(req.body,function(err,user){
                 if(err)
                 {
                     console.log('Error in creating user',err);
                     return;
                 }
+                console.log(user);
                 return res.redirect('/user/login');
             })
         }
@@ -41,6 +67,30 @@ module.exports.create=function(req,res){
 };
 
 module.exports.createSession=function(req,res){
-    
 
+    User.findOne({email : req.body.email}, function(err,user){
+        if(err)
+            {
+                console.log('Error in creating session',err);
+                return;
+            }
+        if(!user)
+        {
+           
+            console.log('User not found');
+        }
+        else
+        {
+            if(req.body.password!=user.password)
+            {
+                console.log('Password wrong');
+                return res.redirect('back');
+            }
+
+            res.cookie('user_id',user.id);
+            return res.redirect('/');
+        }
+    })
 };
+
+
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const Router = express.Router();
 const userController = require('../controllers/users_controller');
 
 Router.get('/login', userController.login);
+Router.get('/profile', userController.profile);
 Router.post('/create', userController.create);
 Router.post('/create-session', userController.createSession);
 
